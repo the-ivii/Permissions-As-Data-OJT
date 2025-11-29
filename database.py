45fda7f511c5279644d471b803fa2480756f637a
Divyanshi <divyanshitalreja12@gmail.com>
2025-11-29 12:13:14 +0530

    Handled databse.py for warnings and updated .gitignore

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Uses a SQLite file for simplicity, easily switched to PostgreSQL later
